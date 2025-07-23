f649abd91db2d4842fa075cf332722afd5bab45d
BagasXece <suk.manyo28@gmail.com>
2025-07-23 11:39:36 +0700

    maap ya

--- question_bank.dart
+++ question_bank.dart
@@ -9,4 +9,44 @@ final List<Map<String, dynamic>> questions = [
     "options": ["A. Nikola Tesla", "B. Thomas Edison", "C. Newton", "D. Einstein"],
     "answer": "B"
   },
+  {
+    "question": "Apa yang dilakukan oleh seorang peneliti?",
+    "options": ["A. Memasak", "B. Membaca", "C. Mencari pengetahuan", "D. Menyanyi"],
+    "answer": "C"
+  },
+  {
+    "question": "Apa yang dilakukan oleh seorang Penyanyi?",
+    "options": ["A. Memasak", "B. Membaca", "C. Mencari pengetahuan", "D. Menyanyi"],
+    "answer": "D"
+  },
+  {
+    "question": "Apa yang dilakukan oleh seorang Koki?",
+    "options": ["A. Memasak", "B. Membaca", "C. Mencari pengetahuan", "D. Menyanyi"],
+    "answer": "A"
+  },
+  {
+    "question": "Berapakah hasil 25 x 4 + 150 : 3?",
+    "options": ["A. 150", "B. 200", "C. 250", "D. 300"],
+    "answer": "A"
+  },
+  {
+    "question": "Proklamasi kemerdekaan indonesia dibacakan pada tanggal?",
+    "options": ["A. 17 Agustus 1945", "B. 28 Oktober 1928", "C. 10 November 1945", "D. 17 Agustus 1946"],
+    "answer": "A"
+  },
+  {
+    "question": "Organ tubuh manusia yang berfungsi memompa darah ke seluruh tubuh adalah?",
+    "options": ["A. Paru-paru", "B. Ginjal", "C. Hati", "D. Jantung"],
+    "answer": "D"
+  },
+  {
+    "question": "Planet ketiga dalam sistem tata surya adalah?",
+    "options": ["A. Mars", "B. Jupiter", "C. Bumi", "D. Venus"],
+    "answer": "C"
+  },
+  {
+    "question": "What is The antonym of 'happy'?",
+    "options": ["A. Joyful", "B. Glad", "C. Sad", "D. Cheerful"],
+    "answer": "C"
+  },
 ];
